e3f2e02b3e1dbebe35e28bf6bad31b25024e5c05
Jared Miller <jared@smell.flowers>
2023-12-22 16:04:35 -0500

    Split update_buffer into smaller methods

--- chad.py
+++ chad.py
@@ -4,10 +4,10 @@ import os
 import signal
 from openai import OpenAI
 # pyright: reportUndefinedVariable=false, reportGeneralTypeIssues=false
-
 """
 chad.py interface a vim buffer with an openai chat completion stream
 """
+
 
 class InterruptedError(Exception):
   pass
@@ -59,28 +59,6 @@ class ChatInterface:
       messages.append({"role": role, "content": "\n".join(content).strip()})
     return messages
 
-  def update_buffer(self, stream):
-    buffer = self.vim.current.buffer
-    line_num = len(buffer) - 1
-
-    for chunk in stream:
-      if chunk.choices[0].delta is None or chunk.choices[
-          0].delta.content is None:
-        continue
-      text = chunk.choices[0].delta.content.replace('\r\n',
-                                                    '\n').replace('\r', '\n')
-      lines = text.split('\n')
-      if buffer[line_num].startswith("### "):
-        line_num += 1
-        buffer[line_num:line_num] = lines
-      else:
-        buffer[line_num] += lines[0]
-        if len(lines) > 1:
-          buffer[line_num + 1:line_num + 1] = lines[1:]
-      line_num += len(lines) - 1
-      self.vim.command("redraw")
-      self.vim.command("normal G")
-
   def run(self, openai_options):
     self.setup_client(openai_options['api_key'], openai_options['model'])
     buffer_content = self.vim.eval('join(getline(1, "$"), "\n")')
@@ -96,6 +74,39 @@ class ChatInterface:
       self.vim.command(f'call append("$", "Error: {error_message}")')
     finally:
       self.vim.command('call append("$", "### user")\nnormal G')
+
+  def update_buffer(self, stream):
+    buffer = self.vim.current.buffer
+    line_num = len(buffer) - 1
+
+    for chunk in stream:
+      if self._should_skip_chunk(chunk):
+        continue
+      text = self._normalize_text(chunk.choices[0].delta.content)
+      lines = text.split('\n')
+      line_num = self._update_buffer_lines(buffer, line_num, lines)
+      self._refresh_display()
+
+  def _should_skip_chunk(self, chunk):
+    return chunk.choices[0].delta is None or chunk.choices[
+        0].delta.content is None
+
+  def _normalize_text(self, content):
+    return content.replace('\r\n', '\n').replace('\r', '\n')
+
+  def _update_buffer_lines(self, buffer, line_num, lines):
+    if buffer[line_num].startswith("### "):
+      line_num += 1
+      buffer[line_num:line_num] = lines
+    else:
+      buffer[line_num] += lines[0]
+      if len(lines) > 1:
+        buffer[line_num + 1:line_num + 1] = lines[1:]
+    return line_num + len(lines) - 1
+
+  def _refresh_display(self):
+    self.vim.command("redraw")
+    self.vim.command("normal G")
 
 
 def main():
